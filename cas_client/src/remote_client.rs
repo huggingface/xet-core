a46048905c0de1e37426a5a48ac97430f2da6edf
Rajat Arya <rajat@huggingface.co>
2024-09-13 17:51:33 -0700

    Rust file reconstruction
    Co-authored by Assaf

--- cas_client/src/remote_client.rs
+++ cas_client/src/remote_client.rs
@@ -1,21 +1,29 @@
+use std::io::{Cursor, Write};
+
 use anyhow::anyhow;
 use bytes::Buf;
 use cas::key::Key;
-use cas_types::compression_scheme::CompressionScheme;
 use cas_types::{
-    QueryChunkResponse, QueryReconstructionResponse, UploadShardResponse, UploadShardResponseType, UploadXorbResponse
+    QueryChunkResponse, QueryReconstructionResponse, UploadXorbResponse
 };
 use reqwest::{StatusCode, Url};
 use serde::{de::DeserializeOwned, Serialize};
 
-use merklehash::MerkleHash;
-use tracing::info;
+use bytes::Bytes;
+use cas_object::cas_object_format::CasObject;
+use cas_types::CASReconstructionTerm;
+use tracing::warn;
 
-use crate::error::{CasClientError, Result};
+use crate::{error::Result, CasClientError};
+
+use merklehash::MerkleHash;
+use tracing::debug;
+
 use crate::Client;
 
-pub const CAS_ENDPOINT: &str = "localhost:4884";
-pub const SCHEME: &str = "localhost:4884";
+pub const CAS_ENDPOINT: &str = "localhost:8080";
+pub const SCHEME: &str = "http:/";
+pub const PREFIX_DEFAULT: &str = "default";
 
 #[derive(Debug)]
 pub struct RemoteClient {
@@ -38,12 +46,12 @@ impl Client for RemoteClient {
             hash: *hash,
         };
 
-        let was_uploaded = self.client
-            .upload(&key, data, chunk_boundaries)
-            .await?;
+        let was_uploaded = self.client.upload(&key, data, chunk_boundaries).await?;
 
         if !was_uploaded {
-            info!("Key: {key:?} not inserted.");
+            debug!("{key:?} not inserted into CAS.");
+        } else {
+            debug!("{key:?} inserted into CAS.");
         }
 
         Ok(())
@@ -53,39 +61,17 @@ impl Client for RemoteClient {
         Ok(())
     }
 
-    async fn get(&self, prefix: &str, hash: &merklehash::MerkleHash) -> Result<Vec<u8>> {
-        // Call reconstruction API
-        let key = Key {
-            prefix: prefix.to_string(),
-            hash: *hash,
-        };
-        let result = self.client.get(&key).await?;
-        Ok(result)
+    async fn get(&self, _prefix: &str, _hash: &merklehash::MerkleHash) -> Result<Vec<u8>> {
+        Err(CasClientError::InvalidArguments)
     }
 
     async fn get_object_range(
         &self,
-        prefix: &str,
-        hash: &merklehash::MerkleHash,
-        ranges: Vec<(u64, u64)>,
+        _prefix: &str,
+        _hash: &merklehash::MerkleHash,
+        _ranges: Vec<(u64, u64)>,
     ) -> Result<Vec<Vec<u8>>> {
-        let key = Key {
-            prefix: prefix.to_string(),
-            hash: *hash,
-        };
-        let xorb = self.client.get(&key).await?;
-        let mut res = Vec::with_capacity(ranges.len());
-        for (start, end) in ranges {
-            let start = start as usize;
-            let end = end as usize;
-            if start > xorb.len() || end > xorb.len() {
-                return Err(CasClientError::InvalidRange);
-            }
-            let section = &xorb[start..end];
-            res.push(Vec::from(section))
-        }
-
-        Ok(res)
+        Err(CasClientError::InvalidArguments)
     }
 
     async fn get_length(&self, prefix: &str, hash: &merklehash::MerkleHash) -> Result<u64> {
@@ -101,8 +87,8 @@ impl Client for RemoteClient {
 }
 
 impl RemoteClient {
-    pub async fn from_config(endpoint: &String) -> Self {
-        Self { endpoint:endpoint.to_string(), client: CASAPIClient::default() }
+    pub async fn from_config(endpoint: String) -> Self {
+        Self { endpoint, client: CASAPIClient::default() }
     }
 }
 
@@ -128,16 +114,8 @@ impl CASAPIClient {
         Self { client, scheme: scheme.to_string(), endpoint: endpoint.to_string() }
     }
 
-    pub async fn get(&self, key: &Key) -> Result<Vec<u8>> {
-        let url = Url::parse(&format!("{0}/{1}/{key}", self.scheme, self.endpoint))?;
-        let request = reqwest::Request::new(reqwest::Method::GET, url);
-        let response = self.client.execute(request).await?;
-        let xorb_data = response.bytes().await?;
-        Ok(xorb_data.to_vec())
-    }
-
     pub async fn exists(&self, key: &Key) -> Result<bool> {
-        let url = Url::parse(&format!("{0}/{1}/{key}", self.scheme, self.endpoint))?;
+        let url = Url::parse(&format!("{0}/{1}/xorb/{key}", self.scheme, self.endpoint))?;
         let response = self.client.head(url).send().await?;
         match response.status() {
             StatusCode::OK => Ok(true),
@@ -149,7 +127,7 @@ impl CASAPIClient {
     }
 
     pub async fn get_length(&self, key: &Key) -> Result<Option<u64>> {
-        let url = Url::parse(&format!("{0}/{1}/{key}", self.scheme, self.endpoint))?;
+        let url = Url::parse(&format!("{0}/{1}/xorb/{key}", self.scheme, self.endpoint))?;
         let response = self.client.head(url).send().await?;
         let status = response.status();
         if status == StatusCode::NOT_FOUND {
@@ -192,30 +170,49 @@ impl CASAPIClient {
             .join(",");
         let url = Url::parse(&format!("{0}/{1}/xorb/{key}?{chunk_boundaries_query}", self.scheme, self.endpoint))?;
 
+        debug!("Upload: POST to {url:?} for {key:?}");
+
         let response = self.client.post(url).body(contents.into()).send().await?;
         let response_body = response.bytes().await?;
         let response_parsed: UploadXorbResponse = serde_json::from_reader(response_body.reader())?;
 
         Ok(response_parsed.was_inserted)
+    }    
+
+    /// Reconstruct a file and write to writer.
+    pub async fn write_file<W: Write>(&self, file_id: &MerkleHash, writer: &mut W) -> Result<usize> {
+
+        // get manifest of xorbs to download
+        let manifest = self.reconstruct_file(file_id).await?;
+
+        self.reconstruct(manifest, writer).await
     }
 
-    pub async fn upload_shard(&self, key: &Key, force_sync: bool, salt: &[u8; 32]) -> Result<bool> {
-        let url = Url::parse(&format!("{0}/{1}/shard/{key}", self.scheme, self.endpoint))?;
-        let response = match force_sync {
-             true => self.client.put(url).send().await?,
-             false => self.client.post(url).send().await?,
-        };
-        let response_body = response.bytes().await?;
-        let response_parsed: UploadShardResponse = serde_json::from_reader(response_body.reader())?;
-
-        match response_parsed.result {
-            UploadShardResponseType::Exists => Ok(false),
-            UploadShardResponseType::SyncPerformed => Ok(true),
+    async fn reconstruct<W: Write>(&self, reconstruction_response: QueryReconstructionResponse, writer: &mut W) -> Result<usize> {
+        let info = reconstruction_response.reconstruction;
+        let total_len = info.iter().fold(0, |acc, x| acc + x.unpacked_length);
+        let futs = info.into_iter().map(|term| {
+            tokio::spawn(async move {
+                let piece = get_one(&term).await?;
+                if piece.len() != (term.range.end - term.range.start) as usize {
+                    warn!("got back a smaller range than requested");
+                }
+                Result::<Bytes>::Ok(piece)
+            })
+        });
+        for fut in futs {
+            let piece = fut
+                .await
+                .map_err(|e| CasClientError::InternalError(anyhow!("join error {e}")))??;
+            writer.write_all(&piece)?;
         }
+        Ok(total_len as usize)
     }
 
-    pub async fn reconstruct_file(&self, file_id: &MerkleHash) -> Result<QueryReconstructionResponse> {
+    /// Reconstruct the file
+    async fn reconstruct_file(&self, file_id: &MerkleHash) -> Result<QueryReconstructionResponse> {
         let url = Url::parse(&format!("{0}/{1}/reconstruction/{2}", self.scheme, self.endpoint, file_id.hex()))?;
+        
         let response = self.client.get(url).send().await?;
         let response_body = response.bytes().await?;
         let response_parsed: QueryReconstructionResponse = serde_json::from_reader(response_body.reader())?;
@@ -244,5 +241,88 @@ impl CASAPIClient {
         let response = self.client.post(url).body(body).send().await?;
         let response_bytes = response.bytes().await?;
         serde_json::from_reader(response_bytes.reader()).map_err(CasClientError::SerdeError)
+    }
+}
+
+async fn get_one(term: &CASReconstructionTerm) -> Result<Bytes> {
+    let url = Url::parse(term.url.as_str())?;
+    let response = reqwest::Client::new()
+        .request(hyper::Method::GET, url)
+        .send()
+        .await?
+        .error_for_status()?;
+    let xorb_bytes = response
+        .bytes()
+        .await
+        .map_err(CasClientError::ReqwestError)?;
+    let mut readseek = Cursor::new(xorb_bytes.to_vec());
+
+    let cas_object = CasObject::deserialize(&mut readseek)?;
+    let data = cas_object.get_range(&mut readseek, term.range.start as u32, term.range.end as u32)?;
+
+    Ok(Bytes::from(data))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use merkledb::{prelude::MerkleDBHighLevelMethodsV1, Chunk, MerkleMemDB};
+    use merklehash::DataHash;
+    use rand::Rng;
+    use tracing_test::traced_test;
+
+    use super::*;
+
+    #[ignore]
+    #[traced_test]
+    #[tokio::test]
+    async fn test_basic_put() {
+        // Arrange
+        let rc = RemoteClient::from_config(CAS_ENDPOINT.to_string()).await;
+        let prefix = PREFIX_DEFAULT;
+        let (hash, data, chunk_boundaries) = gen_dummy_xorb(3, 10248, true);
+
+        // Act
+        let result = rc.put(prefix, &hash, data, chunk_boundaries).await;
+
+        // Assert
+        assert!(result.is_ok());
+    }
+    
+    fn gen_dummy_xorb(num_chunks: u32, uncompressed_chunk_size: u32, randomize_chunk_sizes: bool) -> (DataHash, Vec<u8>, Vec<u64>) {
+        let mut contents = Vec::new();
+        let mut chunks: Vec<Chunk> = Vec::new();
+        let mut chunk_boundaries = Vec::with_capacity(num_chunks as usize);
+        for _idx in 0..num_chunks {
+
+            let chunk_size: u32 = if randomize_chunk_sizes {
+                let mut rng = rand::thread_rng();
+                rng.gen_range(1024..=uncompressed_chunk_size)
+            } else {
+                uncompressed_chunk_size
+            };
+
+            let bytes = gen_random_bytes(chunk_size);
+
+            chunks.push(Chunk { hash: merklehash::compute_data_hash(&bytes), length: bytes.len() });
+
+            contents.extend(bytes);
+            chunk_boundaries.push(contents.len() as u64);
+        }
+
+        let mut db = MerkleMemDB::default();
+        let mut staging = db.start_insertion_staging();
+        db.add_file(&mut staging, &chunks);
+        let ret = db.finalize(staging);
+        let hash = *ret.hash();
+
+        (hash, contents, chunk_boundaries)
+    }
+
+    fn gen_random_bytes(uncompressed_chunk_size: u32) -> Vec<u8> {
+        let mut rng = rand::thread_rng();
+        let mut data = vec![0u8; uncompressed_chunk_size as usize];
+        rng.fill(&mut data[..]);
+        data        
     }
 }
