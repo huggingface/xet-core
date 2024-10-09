4c3eb8b4e05f174ef9847698e3f51cbf4c4dfa40
Rajat Arya <rajat@huggingface.co>
2024-10-09 12:58:18 -0700

    Updated README, removed CachingClient

--- cas_client/src/lib.rs
+++ cas_client/src/lib.rs
@@ -3,7 +3,6 @@
 
 pub use crate::error::CasClientError;
 pub use auth::AuthMiddleware;
-pub use caching_client::CachingClient;
 pub use interface::{Client, ReconstructionClient, UploadClient};
 pub use local_client::tests_utils;
 pub use local_client::LocalClient;
@@ -11,7 +10,6 @@ pub use remote_client::build_reqwest_client;
 pub use remote_client::RemoteClient;
 
 mod auth;
-mod caching_client;
 mod error;
 mod interface;
 mod local_client;
