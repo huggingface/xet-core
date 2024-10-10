6197597bb6430b6ba1745ea8b855cea37df66a51
Rajat Arya <rajat@huggingface.co>
2024-10-10 11:49:08 -0700

    Refactored RemoteClient and LocalClient
    - both are built of empty traits now, no more state
    - moved auth middleware into http_client.rs
    - simplified interface
    
    - next up is to add retry to middleware

--- cas_client/src/remote_client.rs
+++ cas_client/src/remote_client.rs
@@ -1,20 +1,17 @@
 use crate::interface::*;
 use crate::Client;
-use crate::{error::Result, AuthMiddleware, CasClientError};
+use crate::{error::Result, CasClientError};
 use anyhow::anyhow;
 use async_trait::async_trait;
 use bytes::Buf;
 use bytes::Bytes;
 use cas_object::CasObject;
 use cas_types::{CASReconstructionTerm, Key, QueryReconstructionResponse, UploadXorbResponse};
-use error_printer::OptionPrinter;
 use merklehash::MerkleHash;
 use reqwest::{StatusCode, Url};
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, Middleware};
-use retry_strategy::RetryStrategy;
+use reqwest_middleware::ClientWithMiddleware;
 use std::io::{Cursor, Write};
 use tracing::{debug, error, warn};
-use utils::auth::AuthConfig;
 
 pub const CAS_ENDPOINT: &str = "http://localhost:8080";
 pub const PREFIX_DEFAULT: &str = "default";
@@ -26,7 +23,7 @@ fn retry_http_status_code(stat: &reqwest::StatusCode) -> bool {
     stat.is_server_error() || *stat == reqwest::StatusCode::TOO_MANY_REQUESTS
 }
 
-fn is_status_retriable_and_print(err: &reqwest::Error) -> bool {
+fn is_status_retryable_and_print(err: &reqwest::Error) -> bool {
     let ret = err
         .status()
         .as_ref()
@@ -38,25 +35,21 @@ fn is_status_retriable_and_print(err: &reqwest::Error) -> bool {
     ret
 }
 
-fn is_middleware_status_retriable_and_print(err: &reqwest_middleware::Error) -> bool {
+fn is_middleware_status_retryable_and_print(err: &reqwest_middleware::Error) -> bool {
     match err {
-        reqwest_middleware::Error::Reqwest(error) => is_status_retriable_and_print(error),
+        reqwest_middleware::Error::Reqwest(error) => is_status_retryable_and_print(error),
         _ => false,
     }
 }
 
 #[derive(Debug)]
-pub struct RemoteClient {
-    client: ClientWithMiddleware,
-    retry_strategy: RetryStrategy,
-    endpoint: String,
-}
+pub struct RemoteClient {}
 
-// TODO: add retries
 #[async_trait]
 impl UploadClient for RemoteClient {
     async fn put(
-        &self,
+        endpoint: &str,
+        http_auth_client: &ClientWithMiddleware,
         prefix: &str,
         hash: &MerkleHash,
         data: Vec<u8>,
@@ -67,7 +60,14 @@ impl UploadClient for RemoteClient {
             hash: *hash,
         };
 
-        let was_uploaded = self.upload(&key, &data, chunk_and_boundaries).await?;
+        let was_uploaded = RemoteClient::upload(
+            endpoint,
+            http_auth_client,
+            &key,
+            &data,
+            chunk_and_boundaries,
+        )
+        .await?;
 
         if !was_uploaded {
             debug!("{key:?} not inserted into CAS.");
@@ -78,14 +78,19 @@ impl UploadClient for RemoteClient {
         Ok(())
     }
 
-    async fn exists(&self, prefix: &str, hash: &MerkleHash) -> Result<bool> {
+    async fn exists(
+        endpoint: &str,
+        http_auth_client: &ClientWithMiddleware,
+        prefix: &str,
+        hash: &MerkleHash,
+    ) -> Result<bool> {
         let key = Key {
             prefix: prefix.to_string(),
             hash: *hash,
         };
 
-        let url = Url::parse(&format!("{}/xorb/{key}", self.endpoint))?;
-        let response = self.client.head(url).send().await?;
+        let url = Url::parse(&format!("{}/xorb/{key}", endpoint))?;
+        let response = http_auth_client.head(url).send().await?;
         match response.status() {
             StatusCode::OK => Ok(true),
             StatusCode::NOT_FOUND => Ok(false),
@@ -94,26 +99,30 @@ impl UploadClient for RemoteClient {
             ))),
         }
     }
-
-    async fn flush(&self) -> Result<()> {
-        Ok(())
-    }
 }
 
 #[async_trait]
 impl ReconstructionClient for RemoteClient {
-    async fn get_file(&self, hash: &MerkleHash, writer: &mut Box<dyn Write + Send>) -> Result<()> {
+    async fn get_file(
+        endpoint: &str,
+        http_client_auth: &ClientWithMiddleware,
+        http_client: &ClientWithMiddleware,
+        hash: &MerkleHash,
+        writer: &mut Box<dyn Write + Send>,
+    ) -> Result<()> {
         // get manifest of xorbs to download
-        let manifest = self.reconstruct(hash, None).await?;
+        let manifest = RemoteClient::reconstruct(endpoint, http_client_auth, hash, None).await?;
 
-        self.get_ranges(manifest, None, writer).await?;
+        RemoteClient::get_ranges(http_client, manifest, None, writer).await?;
 
         Ok(())
     }
 
     #[allow(unused_variables)]
     async fn get_file_byte_range(
-        &self,
+        endpoint: &str,
+        http_client_auth: &ClientWithMiddleware,
+        http_client: &ClientWithMiddleware,
         hash: &MerkleHash,
         offset: u64,
         length: u64,
@@ -126,27 +135,14 @@ impl ReconstructionClient for RemoteClient {
 #[async_trait]
 impl Reconstructable for RemoteClient {
     async fn reconstruct(
-        &self,
+        endpoint: &str,
+        http_client: &ClientWithMiddleware,
         file_id: &MerkleHash,
         _bytes_range: Option<(u64, u64)>,
     ) -> Result<QueryReconstructionResponse> {
-        let url = Url::parse(&format!(
-            "{}/reconstruction/{}",
-            self.endpoint,
-            file_id.hex()
-        ))?;
+        let url = Url::parse(&format!("{}/reconstruction/{}", endpoint, file_id.hex()))?;
 
-        let response = self
-            .retry_strategy
-            .retry(
-                || async {
-                    let url = url.clone();
-                    self.client.get(url).send().await
-                },
-                is_middleware_status_retriable_and_print,
-            )
-            .await
-            .map_err(|e| CasClientError::InternalError(anyhow!("request failed with code {e}")))?;
+        let response = http_client.get(url).send().await?;
 
         let response_body = response.bytes().await?;
         let response_parsed: QueryReconstructionResponse =
@@ -160,22 +156,14 @@ impl Reconstructable for RemoteClient {
 impl Client for RemoteClient {}
 
 impl RemoteClient {
-    pub fn new(endpoint: &str, auth_config: &Option<AuthConfig>) -> Self {
-        let client = build_reqwest_client(auth_config).unwrap();
-        Self {
-            client,
-            retry_strategy: RetryStrategy::new(NUM_RETRIES, BASE_RETRY_DELAY_MS),
-            endpoint: endpoint.to_string(),
-        }
-    }
-
     pub async fn upload(
-        &self,
+        endpoint: &str,
+        http_client_auth: &ClientWithMiddleware,
         key: &Key,
         contents: &[u8],
         chunk_and_boundaries: Vec<(MerkleHash, u32)>,
     ) -> Result<bool> {
-        let url = Url::parse(&format!("{}/xorb/{key}", self.endpoint))?;
+        let url = Url::parse(&format!("{}/xorb/{key}", endpoint))?;
 
         let mut writer = Cursor::new(Vec::new());
 
@@ -191,7 +179,7 @@ impl RemoteClient {
         writer.set_position(0);
         let data = writer.into_inner();
 
-        let response = self.client.post(url).body(data).send().await?;
+        let response = http_client_auth.post(url).body(data).send().await?;
         let response_body = response.bytes().await?;
         let response_parsed: UploadXorbResponse = serde_json::from_reader(response_body.reader())?;
 
@@ -199,16 +187,17 @@ impl RemoteClient {
     }
 
     async fn get_ranges(
-        &self,
+        http_client: &ClientWithMiddleware,
         reconstruction_response: QueryReconstructionResponse,
         _byte_range: Option<(u64, u64)>,
         writer: &mut Box<dyn Write + Send>,
     ) -> Result<usize> {
         let info = reconstruction_response.reconstruction;
         let total_len = info.iter().fold(0, |acc, x| acc + x.unpacked_length);
-        let futs = info
-            .into_iter()
-            .map(|term| tokio::spawn(async move { get_one_range(&term).await }));
+        let futs = info.into_iter().map(|term| {
+            let http_client_clone = http_client.clone();
+            tokio::spawn(async move { get_one_range(&http_client_clone, &term).await })
+        });
         for fut in futs {
             let piece = fut
                 .await
@@ -219,7 +208,10 @@ impl RemoteClient {
     }
 }
 
-pub(crate) async fn get_one_range(term: &CASReconstructionTerm) -> Result<Bytes> {
+pub(crate) async fn get_one_range(
+    http_client: &ClientWithMiddleware,
+    term: &CASReconstructionTerm,
+) -> Result<Bytes> {
     debug!("term: {term:?}");
 
     if term.range.end < term.range.start || term.url_range.end < term.url_range.start {
@@ -227,27 +219,15 @@ pub(crate) async fn get_one_range(term: &CASReconstructionTerm) -> Result<Bytes>
     }
 
     let url = Url::parse(term.url.as_str())?;
-    let client = reqwest::Client::new();
-    let retry_strategy = RetryStrategy::new(NUM_RETRIES, BASE_RETRY_DELAY_MS);
 
-    let response = retry_strategy
-        .retry(
-            || async {
-                let url = url.clone();
-
-                client
-                    .get(url)
-                    .header(
-                        reqwest::header::RANGE,
-                        format!("bytes={}-{}", term.url_range.start, term.url_range.end - 1),
-                    )
-                    .send()
-                    .await
-            },
-            is_status_retriable_and_print,
+    let response = http_client
+        .get(url)
+        .header(
+            reqwest::header::RANGE,
+            format!("bytes={}-{}", term.url_range.start, term.url_range.end - 1),
         )
-        .await
-        .map_err(|e| CasClientError::InternalError(anyhow!("request failed with code {e}")))?;
+        .send()
+        .await?;
 
     let xorb_bytes = response.bytes().await?;
     if xorb_bytes.len() as u32 != term.url_range.end - term.url_range.start {
@@ -260,46 +240,22 @@ pub(crate) async fn get_one_range(term: &CASReconstructionTerm) -> Result<Bytes>
     Ok(Bytes::from(data))
 }
 
-/// builds the client to talk to CAS.
-pub fn build_reqwest_client(
-    auth_config: &Option<AuthConfig>,
-) -> std::result::Result<ClientWithMiddleware, reqwest::Error> {
-    let auth_middleware = auth_config
-        .as_ref()
-        .map(AuthMiddleware::from)
-        .info_none("CAS auth disabled");
-    let reqwest_client = reqwest::Client::builder().build()?;
-    Ok(ClientBuilder::new(reqwest_client)
-        .maybe_with(auth_middleware)
-        .build())
-}
-
-/// Helper trait to allow the reqwest_middleware client to optionally add a middleware.
-trait OptionalMiddleware {
-    fn maybe_with<M: Middleware>(self, middleware: Option<M>) -> Self;
-}
-
-impl OptionalMiddleware for ClientBuilder {
-    fn maybe_with<M: Middleware>(self, middleware: Option<M>) -> Self {
-        match middleware {
-            Some(m) => self.with(m),
-            None => self,
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
+    use crate::http_client;
+
     use super::*;
     use cas_object::test_utils::*;
     use tracing_test::traced_test;
+    use utils::auth::AuthConfig;
 
-    #[ignore]
+    #[ignore = "requires a running CAS server"]
     #[traced_test]
     #[tokio::test]
     async fn test_basic_put() {
         // Arrange
-        let rc = RemoteClient::new(CAS_ENDPOINT, &None);
+        let auth_config = AuthConfig::maybe_new(None, None, None);
+        let http_client_auth = http_client::build_auth_http_client(&auth_config).unwrap();
         let prefix = PREFIX_DEFAULT;
         let (c, _, data, chunk_boundaries) = build_cas_object(
             3,
@@ -308,9 +264,15 @@ mod tests {
         );
 
         // Act
-        let result = rc
-            .put(prefix, &c.info.cashash, data, chunk_boundaries)
-            .await;
+        let result = RemoteClient::put(
+            CAS_ENDPOINT,
+            &http_client_auth,
+            prefix,
+            &c.info.cashash,
+            data,
+            chunk_boundaries,
+        )
+        .await;
 
         // Assert
         assert!(result.is_ok());
