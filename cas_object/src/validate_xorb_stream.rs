950ff79730c5a8fd06d62c73904e96155e461424
Assaf Vayner <assaf@huggingface.co>
2024-11-27 10:51:21 -0800

    clean

--- cas_object/src/validate_xorb_stream.rs
+++ cas_object/src/validate_xorb_stream.rs
@@ -1,15 +1,15 @@
 use anyhow::anyhow;
 use error_printer::ErrorPrinter;
 use futures::{AsyncRead, AsyncReadExt};
-use tracing::warn;
 use merkledb::prelude::MerkleDBHighLevelMethodsV1;
 use merkledb::{Chunk, MerkleMemDB};
 use merklehash::MerkleHash;
+use std::mem::size_of;
 
 use crate::cas_chunk_format::decompress_chunk_to_writer;
 use crate::cas_object_format::CAS_OBJECT_FORMAT_IDENT;
 use crate::error::{CasObjectError, Result, Validate};
-use crate::{parse_chunk_header, CasObjectInfo};
+use crate::{parse_chunk_header, CASChunkHeader, CasObjectInfo};
 
 // returns Ok(false) on a validation error, returns Err() on a real error
 // returns Ok(true) if no error occurred and the xorb is valid.
@@ -24,7 +24,7 @@ pub async fn validate_cas_object_from_async_read<R: AsyncRead + Unpin>(
 }
 
 async fn _validate_cas_object_from_async_read<R: AsyncRead + Unpin>(reader: &mut R, hash: &MerkleHash) -> Result<()> {
-    let mut chunk_boundary_offsets = vec![];
+    let mut chunk_boundary_offsets: Vec<u32> = Vec::new();
     let mut hash_chunks: Vec<Chunk> = Vec::new();
     let cas_object_info: CasObjectInfo = loop {
         let mut buf8 = [0u8; 8];
@@ -35,32 +35,38 @@ async fn _validate_cas_object_from_async_read<R: AsyncRead + Unpin>(reader: &mut
                 return Err(CasObjectError::FormatError(anyhow!("Xorb Invalid Format Version")));
             }
             // try to parse footer
-            let (cas_object_info, _) =
-                CasObjectInfo::deserialize_async(reader, version).await?;
+            let (cas_object_info, _) = CasObjectInfo::deserialize_async(reader, version).await?;
             break cas_object_info;
         }
 
         // parse the chunk header, decompress the data, compute the hash
         let chunk_header = parse_chunk_header(buf8).log_error(format!("failed to parse chunk header {buf8:?}"))?;
-        let mut compressed_chunk_data = vec![0u8; chunk_header.get_compressed_length() as usize];
+        let chunk_compressed_len = chunk_header.get_compressed_length() as usize;
+        let mut compressed_chunk_data = vec![0u8; chunk_compressed_len];
         reader.read_exact(&mut compressed_chunk_data).await?;
-        let mut uncompressed_chunk_data = Vec::with_capacity(chunk_header.get_uncompressed_length() as usize);
+        let chunk_uncompressed_expected_len = chunk_header.get_uncompressed_length() as usize;
+        let mut uncompressed_chunk_data = Vec::with_capacity(chunk_uncompressed_expected_len);
         decompress_chunk_to_writer(chunk_header, &mut compressed_chunk_data, &mut uncompressed_chunk_data)?;
         let chunk_hash = merklehash::compute_data_hash(&uncompressed_chunk_data);
-        if chunk_header.get_uncompressed_length() as usize != uncompressed_chunk_data.len() {
-            warn!(
-                "chunk at index {} uncompressed len from header: {}, real: {}",
+        if chunk_uncompressed_expected_len != uncompressed_chunk_data.len() {
+            return Err(CasObjectError::FormatError(anyhow!(
+                "chunk at index {} uncompressed len from header: {}, real uncompressed length: {} for xorb: {hash}",
                 hash_chunks.len(),
                 chunk_header.get_uncompressed_length(),
                 uncompressed_chunk_data.len()
-            );
+            )));
         }
 
         hash_chunks.push(Chunk {
             hash: chunk_hash,
             length: uncompressed_chunk_data.len(),
         });
-        chunk_boundary_offsets.push(chunk_boundary_offsets.last().unwrap_or(&0) + compressed_chunk_data.len() as u32);
+        // next offset is computed with: previous offset + length of chunk header + chunk compressed_length
+        chunk_boundary_offsets.push(
+            chunk_boundary_offsets.last().unwrap_or(&0)
+                + size_of::<CASChunkHeader>() as u32
+                + chunk_compressed_len as u32,
+        );
     };
 
     if cas_object_info.cashash != *hash {
