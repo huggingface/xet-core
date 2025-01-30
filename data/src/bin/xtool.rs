acc7adfac7e31abac41db79c5f7c2973933a1bca
seanses <di@huggingface.co>
2025-01-30 12:38:40 -0800

    configure dry run through cli option

--- data/src/bin/xtool.rs
+++ data/src/bin/xtool.rs
@@ -159,6 +159,9 @@ struct DedupArg {
     /// 2: 4 byte groups with LZ4 compression;
     #[clap(short, long)]
     compression: u8,
+    /// Migrate the files by actually uploading them to the CAS server.
+    #[clap(short, long)]
+    migrate: bool,
 }
 
 #[derive(Args)]
@@ -178,15 +181,20 @@ impl Command {
                     hub_client,
                     threadpool,
                     arg.compression.try_into()?,
+                    !arg.migrate,
                 )
                 .await?;
-                let mut writer: Box<dyn Write> = if let Some(path) = arg.output {
-                    Box::new(BufWriter::new(File::options().create(true).write(true).truncate(true).open(path)?))
-                } else {
-                    Box::new(std::io::stdout())
-                };
-                serde_json::to_writer(&mut writer, &all_file_info)?;
-                writer.flush()?;
+
+                // Print file info for analysis
+                if !arg.migrate {
+                    let mut writer: Box<dyn Write> = if let Some(path) = arg.output {
+                        Box::new(BufWriter::new(File::options().create(true).write(true).truncate(true).open(path)?))
+                    } else {
+                        Box::new(std::io::stdout())
+                    };
+                    serde_json::to_writer(&mut writer, &all_file_info)?;
+                    writer.flush()?;
+                }
 
                 eprintln!("\n\nClean results:");
                 for (pf, new_bytes) in clean_ret {
@@ -218,6 +226,7 @@ async fn dedup_files(
     hub_client: HubClient,
     threadpool: Arc<ThreadPool>,
     compression: CompressionScheme,
+    dry_run: bool,
 ) -> Result<(Vec<MDBFileInfo>, Vec<(PointerFile, u64)>, u64)> {
     let token_type = "write";
     let (endpoint, jwt_token, jwt_token_expiry) = hub_client.get_jwt_token(token_type).await?;
@@ -231,7 +240,11 @@ async fn dedup_files(
         default_config(endpoint, Some(compression), Some((jwt_token, jwt_token_expiry)), Some(token_refresher))?;
 
     let num_workers = if sequential { 1 } else { threadpool.num_worker_threads() };
-    let processor = Arc::new(PointerFileTranslator::dry_run(config, threadpool, None, false).await?);
+    let processor = if dry_run {
+        Arc::new(PointerFileTranslator::dry_run(config, threadpool, None, false).await?)
+    } else {
+        Arc::new(PointerFileTranslator::new(config, threadpool, None, false).await?)
+    };
 
     // Scan all files if under recursive mode
     let file_paths = if recursive {
@@ -269,9 +282,12 @@ async fn dedup_files(
 
     let total_bytes_trans = processor.finalize_cleaning().await?;
 
-    let all_file_info = processor.summarize_file_info_of_session().await?;
-
-    Ok((all_file_info, clean_ret, total_bytes_trans))
+    if dry_run {
+        let all_file_info = processor.summarize_file_info_of_session().await?;
+        Ok((all_file_info, clean_ret, total_bytes_trans))
+    } else {
+        Ok((vec![], clean_ret, total_bytes_trans))
+    }
 }
 
 fn is_git_special_files(path: &str) -> bool {
