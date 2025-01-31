f15beb5929fb1e5480dae850af8c3f37afa11922
Hoyt Koepke <hoytak@huggingface.co>
2025-01-31 12:29:15 -1000

    Formatted comment.

--- data/src/data_processing.rs
+++ data/src/data_processing.rs
@@ -194,8 +194,7 @@ impl PointerFileTranslator {
         let new_cas_data = take(cas_data_accumulator.deref_mut());
         drop(cas_data_accumulator); // Release the lock.
 
-        // Note: Only upload if there is new data or info; the file stuff here isn't really
-        //
+        // Upload if there is new data or info
         if !new_cas_data.is_empty() {
             self.xorb_uploader.register_new_cas_block(new_cas_data).await?;
         }
