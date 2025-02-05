83f5a2eb1825ca645b2819f6445267156f4a08d3
Assaf Vayner <assaf@huggingface.co>
2025-02-04 17:27:32 -0800

    iteration on collection

--- xorb-data-collection/src/main.rs
+++ xorb-data-collection/src/main.rs
@@ -1,8 +1,13 @@
 use aws_config::Region;
+use aws_sdk_s3::Client;
+use cas_object::{parse_chunk_header, CasObject, CAS_CHUNK_HEADER_LENGTH};
+use file_utils::SafeFileCreator;
+use futures_util::task::SpawnExt;
 use merklehash::MerkleHash;
-use std::collections::HashMap;
-use std::io::{Read, Write};
+use std::io::{Cursor, Read, Write};
 use std::sync::Arc;
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::task::JoinSet;
 use utils::serialization_utils::{read_hash, read_u32, write_hash, write_u32};
 
 struct XorbEntry {
@@ -20,7 +25,7 @@ impl XorbEntry {
     fn serialize(&self, w: &mut impl Write) {
         write_hash(w, &self.hash).unwrap();
         write_u32(w, self.chunks.len() as u32).unwrap();
-        for chunk in self.chunks {
+        for chunk in &self.chunks {
             write_hash(w, &chunk.hash).unwrap();
             write_u32(w, chunk.uncompressed_len).unwrap();
             write_u32(w, chunk.compressed_len).unwrap();
@@ -46,16 +51,142 @@ impl XorbEntry {
 }
 
 const BUCKET: &str = "xethub-poc-xorb-bucket";
-const XORBS_PREFIX: &str = "xorbs/default/";
+const XORBS_PREFIX: &str = "xorbs/default/00";
+const FILENAME: &str = "xorb.chunks";
 
 #[tokio::main]
 async fn main() {
-    // List xorbs from s3
+    println!("started");
     let sdk_config = aws_config::from_env().region(Region::new("us-east-1")).load().await;
-    let client = Arc::new(aws_sdk_s3::Client::from_conf(aws_sdk_s3::Config::from(&sdk_config)));
+    let s3 = Arc::new(Client::from_conf(aws_sdk_s3::Config::from(&sdk_config)));
 
-    let result = client.list_objects().bucket(BUCKET).prefix(XORBS_PREFIX).send().await.unwrap();
-    result.next_marker
+    let mut js = JoinSet::new();
 
-    // let xorb_result =
+    let (xmd_send, xmd_recv) = tokio::sync::mpsc::channel(200);
+    js.spawn(write_results(xmd_recv));
+
+    let (xkey_send, xkey_recv) = tokio::sync::mpsc::channel(1000);
+    js.spawn(list_bucket(s3.clone(), xkey_send));
+
+    js.spawn(gather_xorb_info(s3.clone(), xkey_recv, xmd_send));
+
+    println!("spawned all, waiting");
+    js.join_all().await;
+}
+
+const NUM_JOBS_CONCURRENT: usize = 100;
+
+async fn gather_xorb_info(s3: Arc<Client>, mut jobs: Receiver<String>, out: Sender<XorbEntry>) {
+    let mut done = false;
+    let mut js = JoinSet::new();
+    for _ in 0..NUM_JOBS_CONCURRENT {
+        if done {
+            break;
+        }
+        if let Some(job) = jobs.recv().await {
+            js.spawn(process_job(s3.clone(), job));
+        } else {
+            done = true;
+        }
+    }
+    while let Some(Ok(entry)) = js.join_next().await {
+        out.send(entry).await.unwrap();
+        if done {
+            continue;
+        }
+        if let Some(job) = jobs.recv().await {
+            js.spawn(process_job(s3.clone(), job));
+        } else {
+            done = true;
+        }
+    }
+    println!("gather_xorb_info done");
+}
+
+async fn process_job(s3: Arc<Client>, job: String) -> XorbEntry {
+    println!("start job: {job}");
+    let xorb_bytes = s3
+        .get_object()
+        .bucket(BUCKET)
+        .key(&job)
+        .send()
+        .await
+        .unwrap()
+        .body
+        .collect()
+        .await
+        .unwrap()
+        .into_bytes();
+
+    let xorb = {
+        let mut rs = Cursor::new(&xorb_bytes);
+        CasObject::deserialize(&mut rs).unwrap()
+    };
+
+    let hash = MerkleHash::from_hex(&job[job.len() - 64..]).unwrap();
+    if xorb.info.num_chunks == 0 {
+        println!("skipping xorb {} since it has 0 chunks; {xorb:?}", hash.hex());
+    }
+
+    let mut chunks = Vec::with_capacity(xorb.info.num_chunks as usize);
+    for (i, chunk_hash) in xorb.info.chunk_hashes.iter().enumerate() {
+        let i = i as u32;
+        let (chunk_header_offset, _) = xorb.get_byte_offset(i, i + 1).unwrap();
+        let chunk_header_offset = chunk_header_offset as usize;
+        let chunk_header_bytes = &xorb_bytes[chunk_header_offset..chunk_header_offset + CAS_CHUNK_HEADER_LENGTH];
+        let chunk_header = parse_chunk_header(chunk_header_bytes.try_into().unwrap()).unwrap();
+        chunks.push(ChunkMD {
+            hash: chunk_hash.clone(),
+            uncompressed_len: chunk_header.get_uncompressed_length(),
+            compressed_len: chunk_header.get_compressed_length(),
+        })
+    }
+    println!("end job: {job}");
+    XorbEntry { hash, chunks }
+}
+
+async fn list_bucket(s3: Arc<Client>, send: Sender<String>) {
+    println!("begin listing bucket");
+    let mut response = s3
+        .list_objects_v2()
+        .max_keys(200)
+        .bucket(BUCKET)
+        .prefix(XORBS_PREFIX)
+        .send()
+        .await
+        .inspect_err(|e| {
+            println!("error listing bucket: {e}");
+        })
+        .unwrap();
+    println!("listed {}, sending...", response.contents().len());
+    for key in response.contents() {
+        send.send(key.key().unwrap().to_string()).await.unwrap();
+    }
+    while let Some(next_continuation_token) = response.next_continuation_token() {
+        response = s3
+            .list_objects_v2()
+            .bucket(BUCKET)
+            .max_keys(200)
+            .prefix(XORBS_PREFIX)
+            .continuation_token(next_continuation_token)
+            .send()
+            .await
+            .unwrap();
+        println!("listed {}, sending...", response.contents().len());
+        for key in response.contents() {
+            send.send(key.key().unwrap().to_string()).await.unwrap();
+        }
+    }
+    println!("list_bucket done");
+}
+
+async fn write_results(mut recv: Receiver<XorbEntry>) {
+    let mut file = SafeFileCreator::new(FILENAME).unwrap();
+    let mut i = 0;
+    while let Some(entry) = recv.recv().await {
+        i += 1;
+        println!("writing results for xorb ({i}) {} num chunks({})", entry.hash.hex(), entry.chunks.len());
+        entry.serialize(&mut file);
+    }
+    println!("write_results done");
 }
