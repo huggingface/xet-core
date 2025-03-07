9a7a6c30e03b4a98e8885da8d8832303d6aa832f
Assaf Vayner <assaf@huggingface.co>
2025-03-07 10:22:56 -0800

    comments and tests

--- cas_object/src/cas_chunk_format/deserialize_async.rs
+++ cas_object/src/cas_chunk_format/deserialize_async.rs
@@ -44,6 +44,7 @@ pub async fn deserialize_chunk_to_writer<R: AsyncRead + Unpin, W: Write>(
     Ok((header.get_compressed_length() as usize + CAS_CHUNK_HEADER_LENGTH, uncompressed_len as u32))
 }
 
+/// deserialize 1 chunk returning a Vec<u8>, the compressed length and the uncompressed length of the chunk
 pub async fn deserialize_chunk<R: AsyncRead + Unpin>(reader: &mut R) -> Result<(Vec<u8>, usize, u32), CasObjectError> {
     let mut buf = Vec::new();
     let (compressed_len, uncompressed_len) = deserialize_chunk_to_writer(reader, &mut buf).await?;
