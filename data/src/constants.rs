242d69fdde544cdc63f8066a3d3b6701aa06b0ee
Hoyt Koepke <hoytak@huggingface.co>
2025-03-20 18:19:17 +0545

    Updated constants.

--- data/src/constants.rs
+++ data/src/constants.rs
@@ -29,4 +29,11 @@ utils::configurable_constants! {
     /// The maximum number of simultaneous xorb upload streams.
     /// The default value is 8 and can be overwritten by environment variable "XET_CONCURRENT_XORB_UPLOADS".
     ref MAX_CONCURRENT_XORB_UPLOADS: usize = 8;
+
+    /// The maximum number of files to ingest at once on the upload path
+    ref MAX_CONCURRENT_FILE_INGESTION: usize = 8;
+
+    /// The maximum number of files to download at one time.
+    ref MAX_CONCURRENT_DOWNLOADS : usize = 8;
+
 }
