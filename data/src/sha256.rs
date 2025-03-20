54e662867fe1f11a350a851932bf4fd9ee4923a5
Hoyt Koepke <hoytak@huggingface.co>
2025-03-20 17:12:06 +0545

    Updated error handling in the sha256 hash.

--- data/src/sha256.rs
+++ data/src/sha256.rs
@@ -3,12 +3,12 @@ use std::sync::Arc;
 use deduplication::Chunk;
 use merklehash::MerkleHash;
 use sha2::{Digest, Sha256};
-use tokio::task::JoinHandle;
+use tokio::task::{JoinError, JoinHandle};
 
 /// Helper struct to generate a sha256 hash as a MerkleHash.
 #[derive(Debug)]
 pub struct ShaGenerator {
-    hasher: Option<JoinHandle<Sha256>>,
+    hasher: Option<JoinHandle<Result<Sha256, JoinError>>>,
 }
 
 impl ShaGenerator {
@@ -23,7 +23,7 @@ impl ShaGenerator {
         // The previous task returns the hasher; we consume that and pass it on.
         self.hasher = Some(tokio::spawn(async move {
             let mut hasher = match current_state {
-                Some(jh) => jh.await.unwrap(),
+                Some(jh) => jh.await??,
                 None => Sha256::default(),
             };
 
@@ -31,7 +31,7 @@ impl ShaGenerator {
                 hasher.update(&chunk.data);
             }
 
-            hasher
+            Ok(hasher)
         }));
     }
 
@@ -46,17 +46,17 @@ impl ShaGenerator {
     }
 
     /// Generates a sha256 from the current state of the variant.
-    pub async fn finalize(mut self) -> MerkleHash {
+    pub async fn finalize(mut self) -> Result<MerkleHash, JoinError> {
         let current_state = self.hasher.take();
 
         let hasher = match current_state {
-            Some(jh) => jh.await.unwrap(),
-            None => return MerkleHash::default(),
+            Some(jh) => jh.await??,
+            None => return Ok(MerkleHash::default()),
         };
 
         let sha256 = hasher.finalize();
         let hex_str = format!("{sha256:x}");
-        MerkleHash::from_hex(&hex_str).unwrap()
+        Ok(MerkleHash::from_hex(&hex_str).unwrap())
     }
 }
 
@@ -75,7 +75,7 @@ mod sha_tests {
     async fn test_sha_generation_builder() {
         let mut sha_generator = ShaGenerator::new();
         sha_generator.update_with_bytes(TEST_DATA.as_bytes());
-        let hash = sha_generator.finalize().await;
+        let hash = sha_generator.finalize().await.unwrap();
 
         assert_eq!(TEST_SHA.to_string(), hash.hex());
     }
@@ -86,7 +86,7 @@ mod sha_tests {
         let td = TEST_DATA.as_bytes();
         sha_generator.update_with_bytes(&td[0..4]);
         sha_generator.update_with_bytes(&td[4..td.len()]);
-        let hash = sha_generator.finalize().await;
+        let hash = sha_generator.finalize().await.unwrap();
 
         assert_eq!(TEST_SHA.to_string(), hash.hex());
     }
@@ -110,7 +110,7 @@ mod sha_tests {
             pos = next_pos;
         }
 
-        let out_hash = sha_generator.finalize().await;
+        let out_hash = sha_generator.finalize().await.unwrap();
 
         let ref_hash = format!("{:x}", Sha256::digest(&rand_data));
 
