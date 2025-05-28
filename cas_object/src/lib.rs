4afdffc58b00f88d76ddbed7e5625b29907d882f
Hoyt Koepke <hoytak@huggingface.co>
2025-05-28 07:42:15 -0700

    Update fmt.

--- cas_object/src/lib.rs
+++ cas_object/src/lib.rs
@@ -2,9 +2,9 @@ pub mod byte_grouping;
 mod cas_chunk_format;
 mod cas_object_format;
 mod compression_scheme;
+pub mod constants;
 pub mod error;
 mod validate_xorb_stream;
-pub mod constants;
 
 pub use cas_chunk_format::*;
 pub use cas_object_format::*;
