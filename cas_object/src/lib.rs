ad1be7ba810d73f49d4700cd88f0eda22c03e4b3
Assaf Vayner <assaf@huggingface.co>
2025-06-02 16:27:49 -0700

    rm validate xorb stream

--- cas_object/src/lib.rs
+++ cas_object/src/lib.rs
@@ -4,9 +4,7 @@ mod cas_object_format;
 mod compression_scheme;
 pub mod constants;
 pub mod error;
-mod validate_xorb_stream;
 
 pub use cas_chunk_format::*;
 pub use cas_object_format::*;
 pub use compression_scheme::*;
-pub use validate_xorb_stream::*;
