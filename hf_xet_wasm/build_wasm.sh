e443ee92609329888aa4ea235b765dba3cc8bf45
Hoyt Koepke <hoytak@huggingface.co>
2026-02-11 12:19:29 -0800

    Upgrade package dependencies (#644)
    This PR updates all the package dependencies that would not cause
    significant API breakages to the current version. The package versions
    in hf_xet_wasm and hf_xet are also updated to match the versions in the
    base package. There should be no functional change.

--- hf_xet_wasm/build_wasm.sh
+++ hf_xet_wasm/build_wasm.sh
@@ -12,7 +12,19 @@ set -ex
 #   features enabled, ensuring that LLVM will generate atomic instructions,
 #   shared memory, passive segments, etc.
 
-RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals \
+WASM_BINDGEN_VERSION="0.2.100"
+
+if command -v wasm-bindgen >/dev/null 2>&1; then
+  INSTALLED_WASM_BINDGEN_VERSION="$(wasm-bindgen --version | awk '{print $2}')"
+else
+  INSTALLED_WASM_BINDGEN_VERSION=""
+fi
+
+if [ "$INSTALLED_WASM_BINDGEN_VERSION" != "$WASM_BINDGEN_VERSION" ]; then
+  cargo install -f wasm-bindgen-cli --version "$WASM_BINDGEN_VERSION"
+fi
+
+TARGET_RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals \
   -C link-arg=--shared-memory \
   -C link-arg=--max-memory=1073741824 \
   -C link-arg=--import-memory \
@@ -21,6 +33,7 @@ RUSTFLAGS="-C target-feature=+atomics,+bulk-memory,+mutable-globals \
   -C link-arg=--export=__tls_align \
   -C link-arg=--export=__tls_base \
   --cfg getrandom_backend=\"wasm_js\"" \
+CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUSTFLAGS="$TARGET_RUSTFLAGS" \
 cargo +nightly build \
     --example simple \
     --target wasm32-unknown-unknown \
@@ -28,7 +41,6 @@ cargo +nightly build \
     -Z build-std=std,panic_abort
 
 # Generate JS/WASM glue for web targets
-RUSTFLAGS='--cfg getrandom_backend="wasm_js"' \
 wasm-bindgen \
     target/wasm32-unknown-unknown/release/examples/simple.wasm \
     --out-dir ./examples/target/ \
